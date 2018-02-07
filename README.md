60d62340e0c19b72a673f5fc5db2a6c3b8ff0ff4
Håkon Krogh <hakonhk@gmail.com>
2018-02-07 09:39:05 +0100

    Added options with stripWWW

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 > HTTPS redirect for an Express webserver running behind a Elastic Load Balancer
 
-# Install
+## Install
 
 ```
 yarn add @crystallize/elasticloadbalancer-express-force-https
 ```
 
-# Usage
+## Usage
 
 ```
 const express = require('express');
 const forceHttps = require('@crystallize/elasticloadbalancer-express-force-https');
 
 const server = express();
-server.use(forceHttps);
+server.use(forceHttps());
 ```
+
+## Options
+
+Pass an optional options object if needed
+
+### stripWWW (boolean)
+
+Optional
+
+Strip the leading www. from the host for both https and http requests
--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 
 /**
  * Will do a 301 redirect if the AWS load balancer adds a x-forwarded-proto to the request headers
+ * options: object
+ * options.stripWWW: boolean
  */
-function forceHttps(req, res, next) {
-  const xfp =
-    req.headers["X-Forwarded-Proto"] || req.headers["x-forwarded-proto"];
-  if (xfp === "http") {
-    const secureUrl = `https://${req.headers.host}${req.url}`;
-    res.redirect(301, secureUrl);
-    res.end();
-    return;
-  }
-  next();
-}
 
-module.exports = forceHttps;
+module.exports = function({ stripWWW = false, addWWW = false }) {
+  function transformHost({ host }) {
+    if (stripWWW && host.match(/^www\./)) {
+      return host.replace(/^www\./, "");
+    }
+    return false;
+  }
+
+  return function forceHttps(req, res, next) {
+    const xfp =
+      req.headers["X-Forwarded-Proto"] || req.headers["x-forwarded-proto"];
+    const host = transformHost(req);
+    if (xfp === "http" || host !== req.host) {
+      res.redirect(301, `https://${host}${req.url}`);
+    } else {
+      next();
+    }
+  };
+};
